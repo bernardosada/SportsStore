3ce205591a064844e5bc6768e5b77061ead1f4ba
BernardoSada <BernardoSada@DESKTOP-JCDFI8T>
2019-11-03 21:41:36 +0000

    Creating a Repository Class The class will getting Product objects fro a database. The class is interface use IEnumerable<T> to allow to obtain a sequence of Product objects.

---
+++ SportsStore/Models/IProductRespository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    interface IProductRespository
+    {
+        IEnumerable<Product> Products { get; }
+
+    }
+}
